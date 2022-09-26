f10a341a2f697e6d58c843c6d7b70a69b2534fb7
Xelcius <mparashar210@gmail.com>
2022-09-26 03:56:30 -0700

    Add files via upload

---
+++ ban.py
@@ -0,0 +1,25 @@
+from pyrogram import Client
+import time
+
+
+ID = "3147700"
+HASH = "e660ea4d20e70a3897aa8cf3a6dc60af" 
+SESS = str(input("\n Send Pyrogram V2 session: "))
+
+RiZoeL = Client(name="RiZoeL", api_id=ID, api_hash=HASH, session_string=SESS)
+
+grp = int(input("Enter Group id: "))
+
+try:
+   RiZoeL.start()
+   for x in RiZoeL.get_chat_members(grp):
+       user = x.user
+       try:
+          RiZoeL.ban_chat_member(grp, user.id)
+          time.sleep(0.01)
+          print("Banned {user.first_name}")
+       except Exception as a: 
+         print(a)
+         continue
+except Exception as a:
+    print(a)
